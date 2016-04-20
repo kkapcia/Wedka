160d11d07c310bb5bc3eff50ed86d0a05503527c
$ [sudo] gem install sass <$ gem install sass>
2016-04-20 12:26:22 +0200

    sticky

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,36 @@
  * License URI:http://support.wrapbootstrap.com/
  * File Description: Place here your custom scripts
  */
+
+$(document).ready(function() {
+        function isScrolledTo(elem) {
+            var docViewTop = $(window).scrollTop(); //num of pixels hidden above current screen
+            var docViewBottom = docViewTop + $(window).height();
+
+            var elemTop = $(elem).offset().top; //num of pixels above the elem
+            var elemBottom = elemTop + $(elem).height();
+
+            return ((elemTop <= docViewTop));
+        }
+
+        var catcher = $('#catcher');
+        var sticky = $('#sticky');
+        var footer = $('#footer');
+
+        $(window).scroll(function() {
+            if(isScrolledTo(sticky)) {
+                sticky.css('position','fixed');
+                sticky.css('top','0px');
+            }
+            var stopHeight = catcher.offset().top + catcher.height();
+            if ( stopHeight > sticky.offset().top) {
+                sticky.css('position','absolute');
+                sticky.css('top',stopHeight);
+            }
+            var bottomStopHeight = footer.offset().top;
+			if ( bottomStopHeight < sticky.offset().top + sticky.height()) { // stop above footer
+                sticky.css('position','absolute');
+                sticky.css('top', bottomStopHeight - sticky.outerHeight() - 10); // a bit of padding
+            }
+        });
+    });
