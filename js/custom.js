50162584bcc961d1e745a072cdb67ef5ae3bb86d
$ [sudo] gem install sass <$ gem install sass>
2016-05-04 15:18:39 +0200

    skonczone formularze, zrobione sortowanie

--- js/custom.js
+++ js/custom.js
@@ -60,6 +60,13 @@ function toggle() {
 
 }
 
-$(function () {
-    $('#datetimepicker1').datetimepicker();
+$(function() {
+    $('input[name="date"]').daterangepicker({
+        singleDatePicker: true,
+        showDropdowns: true
+    },
+    function(start, end, label) {
+        var years = moment().diff(start, 'years');
+        alert("You are " + years + " years old.");
+    });
 });
